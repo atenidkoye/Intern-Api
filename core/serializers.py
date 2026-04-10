3864b37fce0363b8c7cc045abe766b95724dd1d6
Your Name <atenidkoyea@gmail.com>
2026-04-10 14:46:46 +0300

    added views

---
+++ core/serializers.py
@@ -0,0 +1,26 @@
+from rest_framework import serializers
+from .models import Candidate, Application, InterviewNote
+
+
+class CandidateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Candidate
+        fields = '__all__'
+
+
+class ApplicationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Application
+        fields = '__all__'
+        
+
+class InterviewNoteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = InterviewNote
+        fields = '__all__'
+    
+    
+    def Validate_rating(self, value):
+        if value < 1 or value > 5:
+            raise serializers.ValidationError('Rating must not be less than 1')
+        return value
--- core/views.py
+++ core/views.py
@@ -1,3 +1,40 @@
 from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.response import Response
+from django.db.models import Count
+from .models import Candidate, Application, InterviewNote
+from .serializers import CandidateSerializer, ApplicationSerializer,InterviewNoteSerializer
 
 # Create your views here.
+
+class CandidateListCreateView(generics.ListCreateAPIView):
+    queryset = Candidate.objects.all()
+    serializer_class = CandidateSerializer
+
+
+class ApplicationListCreateView(generics.ListCreateAPIView):
+    serializer_class = ApplicationSerializer
+    
+    def get_queryset(self):
+        queryset = Application.objects.all()
+        status = self.request.query_param.get('status')
+        
+        if status:
+            queryset = queryset.filter(status=status)
+        return queryset
+
+
+class InterviewNoteListCreateView(generics.ListCreateAPIView):
+    serializer_class = InterviewNoteSerializer
+    
+    def get_queryset(self):
+        return InterviewNote.objects.filter(application_id=self.kwargs['pk'])
+    
+    def perform_create(self, serializer):
+        serializer.save(application_id=self.kwargs['pk'])
+
+
+class SummaryView(generics.GenericAPIView):
+    def get(self, request):
+        data = Application.objects.values('status').annotate(count=Count('id'))
+        return Response(data)
