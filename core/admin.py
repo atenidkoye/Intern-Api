daafb2d1ae56e28d0b03d6f325183302e7288c29
Your Name <atenidkoyea@gmail.com>
2026-04-10 21:30:19 +0300

    added more config

--- core/admin.py
+++ core/admin.py
@@ -2,8 +2,9 @@ from django.contrib import admin
 from django.contrib import admin
 from .models import Candidate, Application, InterviewNote
 
+
+# Register your models here.
+
 admin.site.register(Candidate)
 admin.site.register(Application)
 admin.site.register(InterviewNote)
-
-# Register your models here.
